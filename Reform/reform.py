42b53291d17f074e858be3932a48a026680da003
ilaysali <ilaysali123@gmail.com>
2025-12-28 22:46:58 +0200

    feature reform: make reform syntax more readable

--- Reform/reform.py
+++ Reform/reform.py
@@ -1,24 +1,5 @@
 from Exceptions import NumbersInARowException
-
-
-def is_number(token: str) -> bool:
-    """
-    Checks if the string token can be converted to a float.
-
-    Args:
-        token (str): The string to check.
-
-    Returns:
-        bool: True if the token is a valid number.
-    """
-
-    try:
-        if (token[0] == '+' or token[0] == '-'):
-            return False
-        float(token)
-        return True
-    except ValueError:
-        return False
+from utility import is_number
 
 
 def remove_whitespace(expression: str) -> str:
@@ -27,7 +8,7 @@ def remove_whitespace(expression: str) -> str:
     contain two consecutive numbers.
 
     Args:
-        expression (str): The mathematical expression to validate.
+        expression (str): The mathematical expression.
 
     Returns:
         str: The expression with all whitespace removed.
@@ -38,33 +19,55 @@ def remove_whitespace(expression: str) -> str:
 
     divided_by_whitespace = expression.split()
     is_previous_number = False
+
     for token in divided_by_whitespace:
+
         if is_number(token):
             if is_previous_number:
                 raise NumbersInARowException("Invalid syntax: Two numbers in a row")
             is_previous_number = True
+
         else:
             is_previous_number = False
 
     return expression.replace(' ', '')
 
 def reform_sequence_of_intensity1 (expression: str) -> str:
+    """
+        Replaces sequence of -,+ via mathematical logic:
+            even number of - becomes +
+            odd number of - becomes -
+            sequence of + becomes +
+
+        Args:
+            expression (str): The mathematical expression.
+
+        Returns:
+            str: The expression with replaced sequence of -,+ via mathematical logic.
+    """
+
     reformed_expression = []
     minus_count = 0
     sequence = False
+
     for token in expression:
         if token == '-' or token == '+':
             if token == '-':
                 minus_count += 1
             sequence = True
+
         else:
             if sequence:
                 if minus_count % 2 == 0:
                     reformed_expression.append("+")
+
                 else:
                     reformed_expression.append("-")
+
                 sequence = False
                 minus_count = 0
+
             reformed_expression.append(token)
+
     return ''.join(reformed_expression)
 
